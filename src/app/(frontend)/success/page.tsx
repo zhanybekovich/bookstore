cbb8ccba7e27983dde78f4189540e459d45bf2b7
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-04-22 09:15:17 +0600

    add full finished success payment page

--- src/app/(frontend)/success/page.tsx
+++ src/app/(frontend)/success/page.tsx
@@ -3,6 +3,7 @@
 import { useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 import { useCartStore } from '@/store/useCartStore'
+import Link from 'next/link'
 
 export default function SuccessPage() {
   const params = useSearchParams()
@@ -16,10 +17,19 @@ export default function SuccessPage() {
   }, [sessionId])
 
   return (
-    <div className="p-10 text-center">
-      <h1 className="text-3xl font-bold text-green-600">Payment successful 🎉</h1>
+    <div className="p-10 text-center min-h-[calc(100vh-123px-116px)] flex flex-col items-center justify-center">
+      <div>
+        <h1 className="text-3xl font-bold text-green-600 mb-4">Payment successful 🎉</h1>
 
-      <p className="mt-4 text-sm text-gray-500">Session: {sessionId}</p>
+        <p className="text-gray-500 mb-8">Thank you for your purchase! We'll contact you soon.</p>
+
+        <Link
+          href="/orders"
+          className="inline-block py-3 px-8 bg-indigo-600 text-white rounded-full"
+        >
+          My Orders
+        </Link>
+      </div>
     </div>
   )
 }
