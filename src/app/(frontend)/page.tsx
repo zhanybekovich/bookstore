f7ea93a139414ba1c41f8e25e50b1a1a2d8b6a27
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-04-22 10:52:50 +0600

    add title on home page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,12 +2,18 @@ import CategoryCard from '@/components/CategoryCard'
 import HomepageHeroBlock from '@/components/HomepageHeroBlock'
 import ProductCard from '@/components/ProductCard'
 import { getCategories, getHomeProducts, getStoreInfo } from '@/lib/apiServices'
-import Image from 'next/image'
-
 import { getVeryLightColor } from '@/lib/utils'
-
 import Link from 'next/link'
 import { FaArrowRightLong } from 'react-icons/fa6'
+import { Metadata } from 'next'
+
+export async function generateMetadata(): Promise<Metadata> {
+  const store = await getStoreInfo()
+
+  return {
+    title: `${store?.name} - Online Store` || 'Home Page',
+  }
+}
 
 export default async function HomePage() {
   const store = await getStoreInfo()
